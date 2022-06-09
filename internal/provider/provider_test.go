4c1a62b137e3f3e4818f02fc859ba39aa5177d04
sheneska.williams <sheneska.williams@hashicorp.com>
2022-06-09 16:49:44 -0400

    Fix breaking changes

--- internal/provider/provider_test.go
+++ internal/provider/provider_test.go
@@ -4,13 +4,13 @@ import (
 	"os"
 	"testing"
 
-	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
+	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
 )
 
 var providerFactories = map[string]func() (tfprotov6.ProviderServer, error){
 	"salesforce": func() (tfprotov6.ProviderServer, error) {
-		return tfsdk.NewProtocol6Server(New()), nil
+		return providerserver.NewProtocol6WithError(New())()
 	},
 }
 
